e626d2e849a418999d343e6507d92747d811ca51
Felipe Molinari <felipe.molinari@yahoo.com.br>
2023-09-01 01:01:13 +0100

    Criando um novo codigo limpo

---
+++ newcode.py
@@ -0,0 +1,10 @@
+# Limpando o codigo
+
+# inputs 
+nome = str(input("Digite seu nome "))
+idade = int(input("Digite sua idade "))
+peso = int(input("Digite seu peso "))
+altura = int(input("Digite sua altura "))
+
+
+
